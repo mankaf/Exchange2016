1d647adfadcdb3f79966793d2a1a34be94601e84
Miroslav Toman <miro.toman@gmail.com>
2024-06-16 21:58:21 +0200

    Update upgrade-ad-schema-CU23.bat

--- upgrade-ad-schema-CU23.bat
+++ upgrade-ad-schema-CU23.bat
@@ -1,3 +1,5 @@
+#enter to Admin command prompt
+
 Setup.exe /PrepareSchema /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
 Setup.exe /PrepareAD /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
 Setup.exe /PrepareAllDomains /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
