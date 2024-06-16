8184ff5a139895d647573358cc29abd6fcdec148
Miroslav Toman <miro.toman@gmail.com>
2024-06-16 21:53:48 +0200

    Create upgrade-ad-schema.bat

---
+++ upgrade-ad-schema.bat
@@ -0,0 +1,3 @@
+Setup.exe /PrepareSchema /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
+Setup.exe /PrepareAD /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
+Setup.exe /PrepareAllDomains /IAcceptExchangeServerLicenseTerms_DiagnosticDataON
